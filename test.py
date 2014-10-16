2ceed994ac9ba541682cc91e22bc9b3cc7bd1ab3
andrewaeva@ya.ru <123321223a>
2014-10-16 14:18:09 +0400

    4 zad

--- test.py
+++ test.py
@@ -9,18 +9,19 @@ X = np.loadtxt('diabetes.data', skiprows=1)
 y = X[:, -1]
 X = X[:, 0:-1]
 
-ytrain, ytest = y[0:200], y[200:422]
+ytrain_0, ytest_0 = y[0:200], y[200:422]
 Xtrain, Xtest = X[0:200], X[200:422]
 
 Xbar = np.mean(Xtrain, axis=0) # среднее
 Xstd = np.std(Xtrain, axis=0) # отклонение от среднего
-ybar = np.mean(ytrain) # среднее
-ytrain = ytrain - ybar
+ybar = np.mean(ytrain_0) # среднее
+ytrain = ytrain_0 - ybar
 Xtrain = (Xtrain - Xbar) / Xstd # нормирование
 
-
-alphas = [i for i in xrange(1, 10000)]
+alphas = [i for i in xrange(0, 10000)]
 clf = Ridge(fit_intercept=False)
+
+Plote = []
 
 coefs = []
 error = []
@@ -29,8 +30,11 @@ for a in alphas:
     clf.fit(Xtrain, ytrain)
     coefs.append(clf.coef_)
     C = np.array(clf.coef_)
-    E = np.dot(ytrain - np.dot(Xtrain, C).transpose(), ytrain - np.dot(Xtrain, C))
+    E = np.linalg.norm(ybar - np.dot(Xtest, C)) / np.linalg.norm(ytrain)
     error.append(E)
+
+Plote.append(error)
+
 ax = plt.gca()
 ax.plot(alphas, coefs)
 ax.set_xscale('log')
@@ -38,34 +42,17 @@ plt.xlabel('lambda')
 plt.title('Ridge  with regulization')
 plt.show()
 
-Plote = []
-Plote.append(error[:100]) #график ошибки тренир
 
-Xbar = np.mean(Xtest, axis=0) # среднее
-Xstd = np.std(Xtest, axis=0) # отклонение от среднего
-ybar = np.mean(ytest) # среднее
-ytest = ytest - ybar
-Xtest = (Xtest - Xbar) / Xstd # нормирование
-
-
-alphas = [i for i in xrange(1, 10000)]
-clf = Ridge(fit_intercept=False)
-
-coefs = []
 error = []
 for a in alphas:
-    clf.set_params(alpha=a)
-    clf.fit(Xtest, ytest)
-    coefs.append(clf.coef_)
-    C = np.array(clf.coef_)
-    E = np.dot(ytest - np.dot(Xtest, C).transpose(), ytest - np.dot(Xtest, C))
+    E = np.linalg.norm(ytest_0 - ybar - np.dot(Xtest, coefs[a])) / np.linalg.norm(ytrain)
     error.append(E)
 
+Plote.append(error) # график ошибки тестов
 
-Plote.append(error[:100]) # график ошибки тестов
 ax = plt.gca()
-ax.plot(alphas[:100], Plote[0])
-ax.plot(alphas[:100], Plote[1])
+ax.plot(alphas, Plote[0])
+ax.plot(alphas, Plote[1])
 ax.set_xscale('log')
 plt.xlabel('lambda')
 plt.title('Error')
