b77239cfcd14f7a82499a2cdae93afe0f46b4383
andrewaeva@ya.ru <123321223a>
2014-10-17 11:14:36 +0400

    Fix error

--- test.py
+++ test.py
@@ -9,8 +9,8 @@ X = np.loadtxt('diabetes.data', skiprows=1)
 y = X[:, -1]
 X = X[:, 0:-1]
 
-ytrain_0, ytest_0 = y[0:200], y[200:422]
-Xtrain, Xtest = X[0:200], X[200:422]
+ytrain_0, ytest_0 = y[0:220], y[220+1:442+1]
+Xtrain, Xtest = X[0:220], X[220+1:442+1]
 
 Xbar = np.mean(Xtrain, axis=0) # среднее
 Xstd = np.std(Xtrain, axis=0) # отклонение от среднего
@@ -42,10 +42,6 @@ plt.xlabel('lambda')
 plt.title('Ridge  with regulization')
 plt.show()
 
-
-
-Xbar = np.mean(Xtest, axis=0) # среднее
-Xstd = np.std(Xtest, axis=0) # отклонение от среднего
 Xtest = (Xtest - Xbar) / Xstd # нормирование
 
 error = []
@@ -104,10 +100,6 @@ plt.xlabel('lambda')
 plt.title('Ridge  with regulization')
 plt.show()
 
-
-
-Xbar = np.mean(Xtest, axis=0) # среднее
-Xstd = np.std(Xtest, axis=0) # отклонение от среднего
 Xtest = (Xtest - Xbar) / Xstd # нормирование
 
 error = []
